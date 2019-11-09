6684995dd464021ec06e1d1ab84e9dcf5946b8b8
root <root@vps230347.vps.ovh.ca>
2019-11-08 22:54:41 -0500

    Google auth working

--- static/main.js
+++ static/main.js
@@ -1,4 +1,6 @@
 socket = io();
+var guser;
+var profile;
 
 
 // socket.emit("location", {
@@ -9,3 +11,31 @@ socket = io();
 socket.on("update", obj => {
     $("#update").text(obj.count)
 });
+
+function onSignIn(googleUser) {
+    guser = googleUser.getAuthResponse();
+    profile = googleUser.getBasicProfile();
+    console.log("EEE", guser)
+    var id_token = guser.id_token;
+    
+
+    $.ajax({
+        method: "POST",
+        url: 'https://atischool.net/glogin',
+        data: {idtoken: id_token}
+    }).done(function() {
+        console.log("login successful")
+        $("#logout").show()
+    }).fail(function(e) {
+        console.log("login failed", e)
+        // window.location.href = "/"
+    });
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        window.location.href = "/logout"
+    });
+}
--- webserver.py
+++ webserver.py
@@ -6,6 +6,8 @@ import json
 import sys
 import sqlite3
 from sqlite3 import Error
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'schloop'
@@ -17,41 +19,70 @@ Session(app)
 
 users = {}
 
+conn = None
+
 
 @app.route("/")
 def slash():
-    token = session.get("token", False)
-    count = session.get("token", False)
-
-    if not token:
-        token = str(uuid.uuid4())
-        session["token"] = token
-
-    if not count:
-        session["count"] = 0
-
     return render_template("home.html")
 
-@app.route("/update")
-def update():
-    count = session.get("count", False)
-    token = session.get("token", False)
 
-    print(count, token)
+@app.route("/glogin", methods=["POST"])
+def login():
+    idtoken = request.form.get('idtoken', False)
+    userid = session.get("userid", False)
 
-    if token is False or count is False:
-        return redirect("/", 302)
+    if not idtoken:
+        session.clear()
+        return "error", 500
+
+    if userid:
+        return "success"
     else:
-        session["count"] = session.get("count") + 1
-        socketio.emit(
-            "update", {
-                "count": count
-            },
-            room=token,
-            json=True)
+        cur = conn.cursor()
+        credentials = getcreds(idtoken)
+        hd = credentials.get("hd", False)
+        email = credentials.get("email", False)
+        name = credentials.get("name", False)
 
 
-    return "done"
+        if hd != "alt.app":
+            print("Not ATI google account")
+            session.clear()
+            return "notati", 403
+
+        if not credentials:
+            console.log("loginerr")
+            return "loginerror", 403
+
+        cur.execute('SELECT * FROM users WHERE email=?', (email,))
+
+        userdata = cur.fetchone()
+
+        if not userdata:
+            userid = uuid.uuid4()
+            session["userid"] = str(userid)
+
+            print("User doesnt exist")
+
+            cur.execute("INSERT INTO users VALUES (?,?,?,1)", (email, name, str(userid),))
+        else:
+            print("User exists")
+            session["userid"] = userdata[2]
+            
+        session["email"] = email
+        session["name"] = name
+        
+        conn.commit()
+
+        print("SUCCESS")
+        return "success"
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 # @socketio.on("location")
@@ -75,19 +106,33 @@ def connect():
     if token:
         join_room(token)
 
-
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
+def getcreds(idtoken):
     try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+        # Specify the CLIENT_ID of the app that accesses the backend:
+        idinfo = id_token.verify_oauth2_token(idtoken, requests.Request(), "203450520052-4olsv1k1uj6ditok97qncbho9n8usk36.apps.googleusercontent.com")
+
+        # Or, if multiple clients access the backend server:
+        # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+        # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+        #     raise ValueError('Could not verify audience.')
+
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+
+        # If auth request is from a G Suite domain:
+        # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+        #     raise ValueError('Wrong hosted domain.')
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        print("IDINFO", idinfo)
+
+        return idinfo
+    except ValueError:
+        return False
+        # Invalid token
+        pass
 
 if __name__ == "__main__":
-    create_connection(r"sqlite.db")
-    socketio.run(app, debug=True, host="localhost")
+    conn = sqlite3.connect('sqlite.db', check_same_thread=False)
+    socketio.run(app, debug=True, host="localhost", port="6405")
